23a47a09a70424a02e06ee8196281e38c252a2a5
Arcuzz <140261284+Arcuzz@users.noreply.github.com>
2023-12-21 17:30:59 +0100

    Add files via upload

--- Questions.java
+++ Questions.java
@@ -1,17 +1,28 @@
+package pac;
 import java.util.*;
 import java.io.*;
 public class Questions {
     public ArrayList<Prova> domande = new ArrayList<>();
+    public int piano;
+    public String tema;
+    public String difficolta;
+    public int rank;
+    public int numDomande;
     public Questions(){}
-    public Questions(int numOfQuestions, int floor) throws FileNotFoundException {
-        int totalQuestions = countTotalQuestions(floor);
-        System.out.println("\nScelto " + numOfQuestions + " domande su " + totalQuestions + " per il piano " + floor);
-        addQuestions(numOfQuestions ,floor);
+    public Questions(int livello, String tema, String diff) throws FileNotFoundException {
+        this.piano = livello;
+        this.tema = tema;
+        this.difficolta = diff;
+        this.numDomande = livello+livello+1;
+        setRank();
+        int totalQuestions = countTotalQuestions();
+        System.out.println("\nScelto " + numDomande + " domande su " + totalQuestions + " per il piano " + livello);
+        addQuestions();
         //printProve();
     }
 
-    public void addQuestions(int number, int floor) throws FileNotFoundException {
-        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\Gioco\\src\\Matematica\\enigmi"+String.valueOf(floor)+".txt");
+    public void addQuestions() throws FileNotFoundException {
+        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\Gioco\\src\\"+this.tema+"\\enigmi"+this.rank+".txt");
         Scanner scan = new Scanner(file);
         while(scan.hasNextLine()){
             String dom = scan.nextLine();
@@ -19,24 +30,33 @@ public class Questions {
             String hint = scan.nextLine();
             domande.add(new Prova(dom,ris,hint,0));
         }
+        scan.close();
         Collections.shuffle(domande);
-        domande.removeIf(n -> (domande.indexOf(n) >= number)); //solo con > ne mette uno in più
+        domande.removeIf(n -> (domande.indexOf(n) >= numDomande)); //solo con > ne mette uno in più
     }
 
-    public int countTotalQuestions(int floor) throws FileNotFoundException {
-        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\Gioco\\src\\Matematica\\enigmi"+String.valueOf(floor)+".txt");
+    public int countTotalQuestions() throws FileNotFoundException {
+        File file = new File("C:\\Users\\ASUS\\IdeaProjects\\Gioco\\src\\"+this.tema+"\\enigmi"+this.rank+".txt");
         Scanner scan = new Scanner(file);
         int count = 0;
         while(scan.hasNextLine()) {
             count++;
             scan.nextLine();
         }
-        return count/2;
+        scan.close();
+        return count/3;
     }
 
     public void printProve(){
         for(Prova item : domande)
             item.summary();
     }
-
+    public void setRank(){
+        switch (this.difficolta) {
+            case "facile" -> this.rank = 1;
+            case "media" -> this.rank = 2;
+            case "difficile" -> this.rank = 3;
+            case "crescente" -> this.rank = this.piano;
+        }
+    }
 }

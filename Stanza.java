0e1fc63eeb1b571e0150814fd0fe5c582e34c927
GiOvPa <127603823+GiOvPa@users.noreply.github.com>
2024-01-02 12:44:28 +0100

    Update Stanza.java

--- Stanza.java
+++ Stanza.java
@@ -1,5 +1,7 @@
 package pac;
 
+import java.util.Scanner;
+
 public class Stanza {
     public char id;
 
@@ -17,17 +19,37 @@ class Domanda extends Stanza{
         this.prova = prova;
         this.risposta = false;
     }
+
+    public void idle(Scanner scan){
+        System.out.println(this.prova.domanda);
+        while (!this.risposta) {
+            String in = scan.nextLine();
+            if (in.equals(this.prova.risposta)) this.risposta = true;
+            else System.out.println("Risposta sbagliata, riprova");
+        }
+    }
 }
 
 class Npc extends Stanza{
-    // public Personaggio npc;
     public String nome;
     public Minigiochi mini;
+    public boolean res;
 
     public Npc(String nome, Minigiochi mini){
         super('N');
         this.nome = nome;
         this.mini = mini;
+        this.res = false;
+    }
+
+    public void idle(Scanner scan){
+        System.out.println("Sono "+this.nome+" risolvi il mio minigioco");
+        while(!this.res){
+            this.mini.inizializza();
+            this.mini.startGame();
+            this.mini.play(scan);
+            this.res = true;
+        }
     }
 }
 

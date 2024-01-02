e0d04e5b3eee2d0eda86aabb490ffb8ddf8f4788
GiOvPa <127603823+GiOvPa@users.noreply.github.com>
2024-01-02 12:44:06 +0100

    Update Torre.java

--- Torre.java
+++ Torre.java
@@ -1,5 +1,7 @@
 package pac;
 
+import java.util.Scanner;
+
 public class Torre {
 
     public int livello = 1;
@@ -14,19 +16,19 @@ public class Torre {
         this.pro = pro;
     }
 
-    public void game(){a
-        this.pianoCurr = new Piano(this.livello);
+    public void game(Scanner scan){
+        this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, "Matematica");
         pro.start(this.pianoCurr);
         while (this.livello <= this.diff.numPiani) {
-            
-            pro.move();
-            if(this.pianoCurr.dom_sup == this.pianoCurr.n_dom){
+            pro.move(scan);
+            if(this.pianoCurr.dom_sup == this.pianoCurr.n_dom && this.livello+1 <= this.diff.numPiani){
                 this.livello ++;
-                this.pianoCurr = new Piano(this.livello);
-                if(this.pianoCurr.n_npc != 0) pro.start(this.pianoCurr);
-                else break;
+                this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, "Matematica");
+                pro.start(this.pianoCurr);
+            }else{
+                System.out.println("Sei uscito dal gioco");
+                break;
             }
         }
-        System.out.println("HAI VINTO !!!!");
     }
 }

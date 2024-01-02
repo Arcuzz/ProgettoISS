abf71c47dffca44a64a2525578e1ab957004cc26
Arcuzz <140261284+Arcuzz@users.noreply.github.com>
2024-01-02 15:21:49 +0100

    Update Torre.java
    Aggiornato temi a un arraylist, così viene disordinato dalla torre dopo che il suo costruttore riceve un array normale

--- Torre.java
+++ Torre.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class Torre {
 
     public int livello = 1;
-    public String[] temi;
+    public ArrayList<String> temi = new ArrayList<>();
     public Difficulty diff;
     public Piano pianoCurr;
     public Protagonista pro;
 
     public Torre(String[] temi, Difficulty diff, Protagonista pro){
-        this.temi = temi;
+        Collections.addAll(this.temi,temi);
+        Collections.shuffle(this.temi);
         this.diff = diff;
         this.pro = pro;
     }
 
     public void game(Scanner scan){
-        this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, "Matematica");
+        this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, this.temi.get(this.livello);
         pro.start(this.pianoCurr);
         while (this.livello <= this.diff.numPiani) {
             pro.move(scan);
             if(this.pianoCurr.dom_sup == this.pianoCurr.n_dom && this.livello+1 <= this.diff.numPiani){
                 this.livello ++;
-                this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, "Matematica");
+                this.pianoCurr = new Piano(this.livello, this.diff.difficoltà, this.temi.get(this.livello));
                 pro.start(this.pianoCurr);
             }else{
                 System.out.println("Sei uscito dal gioco");

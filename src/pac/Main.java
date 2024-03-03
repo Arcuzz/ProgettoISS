a9c67dc96dca8c04c0bca81636fabe9bfd0e53e0
GiOvPa <127603823+GiOvPa@users.noreply.github.com>
2024-03-03 18:37:04 +0100

    Update Main.java

--- src/pac/Main.java
+++ src/pac/Main.java
@@ -1,14 +1,21 @@
 package pac;
 
+import pac.minigiochi.*;
+
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        MenuModel menu = new MenuModel();
+        MemoryModel mini = new MemoryModel();
+        mini.rank = 2;
+        mini.inizializza();
+        MemoryController co = new MemoryController(mini, new MemoryView());
+        co.play(scan, "ASD");
+        /*MenuModel menu = new MenuModel();
         MenuController menuController = new MenuController(menu, new MenuView());
-        menuController.display(scan);
+        menuController.display(scan);*/
         scan.close();
     }
 

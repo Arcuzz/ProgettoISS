48003d4ca4edfb32473912211c74476ff06da298
GiOvPa <127603823+GiOvPa@users.noreply.github.com>
2024-03-19 17:20:43 +0100

    Update ImpiccatoController.java

--- src/pac/minigiochi/ImpiccatoController.java
+++ src/pac/minigiochi/ImpiccatoController.java
@@ -19,28 +19,28 @@ public class ImpiccatoController extends MinigiocoController implements Serializ
         view.initGraphic(nome);
         sca.nextLine();
         String in = "";
-        view.startGame();
+        view.startGame(model.remainingAttempts);
         if (sca.nextLine().equalsIgnoreCase("n")) return false;
 
-        while (model.remainingAttempts > 0 && !model.secret.equalsIgnoreCase(model.guessed.toString()) && !in.equals("exit")) {
+        while (model.remainingAttempts > 0 && !model.getSecret().equalsIgnoreCase(model.guessed.toString()) && !in.equals("exit")) {
 
             view.showGame(model.guessed, model.remainingAttempts);
             in = sca.nextLine();
             
             if(in.length() > 1){
-                if (in.equalsIgnoreCase(model.secret)){
+                if (in.equalsIgnoreCase(model.getSecret())){
+                    model.guessed.setLength(0);
+                    model.guessed.append(model.getSecret());
                     view.showGame(model.guessed, model.remainingAttempts);
                     view.completedGame();
-                    model.guessed.setLength(0);
-                    model.guessed.append(model.secret);
                     view.resumeGame();
                     sca.nextLine();
                     return true;
-                }else if (model.secret.toLowerCase().contains(in.toLowerCase())){
-                    int init_pos = model.secret.toLowerCase().indexOf(in.toLowerCase());
+                }else if (model.getSecret().toLowerCase().contains(in.toLowerCase())){
+                    int init_pos = model.getSecret().toLowerCase().indexOf(in.toLowerCase());
                     for(int i = init_pos; i < init_pos + in.length(); i++){
-                        model.guessed.setCharAt(i, model.secret.charAt(i));
-                    }if (model.secret.equalsIgnoreCase(model.guessed.toString())){
+                        model.guessed.setCharAt(i, model.getSecret().charAt(i));
+                    }if (model.getSecret().equalsIgnoreCase(model.guessed.toString())){
                         view.showGame(model.guessed, model.remainingAttempts);
                         view.completedGame();
                         view.resumeGame();
@@ -50,11 +50,11 @@ public class ImpiccatoController extends MinigiocoController implements Serializ
 
                 }else if(!in.equalsIgnoreCase("exit"))model.remainingAttempts -= in.length();
             }else{
-                if(model.secret.contains(in.toLowerCase())){
-                    for(int i = 0; i < model.secret.length(); i++){
-                        if(String.valueOf(model.secret.charAt(i)).equalsIgnoreCase(in.toLowerCase())) model.guessed.setCharAt(i, in.charAt(0));
+                if(model.getSecret().contains(in.toLowerCase())){
+                    for(int i = 0; i < model.getSecret().length(); i++){
+                        if(String.valueOf(model.getSecret().charAt(i)).equalsIgnoreCase(in.toLowerCase())) model.guessed.setCharAt(i, in.charAt(0));
                     }
-                    if (model.secret.equalsIgnoreCase(model.guessed.toString())){
+                    if (model.getSecret().equalsIgnoreCase(model.guessed.toString())){
                         view.showGame(model.guessed, model.remainingAttempts);
                         view.completedGame();
                         view.resumeGame();
